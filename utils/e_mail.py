dfb7fc455f8e030c7f01684b11b5b049c5243d40
krishbhardwaj075-source <your_github_email@example.com>
2026-04-17 12:41:37 +0530

    new changes

--- utils/e_mail.py
+++ utils/e_mail.py
@@ -1,14 +1,22 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+load_dotenv()
 def send_otp(email,otp):
-    sender="krishbhardwaj.075@gmail.com"
-    password="xgjo ueqc avlx qqgp"
-    message=MIMEText(f"Your OTP for verification: {otp}")
-    message["subject"]="AI Study Planner OTP"
-    message["From"]=sender
-    message["To"]=email
-    server=smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()
-    server.login(sender,password)
-    server.sendmail(sender,email,message.as_string())
-    server.quit()
+    try:
+        sender=os.getenv("EMAIL_SENDER")
+        password=os.getenv("EMAIL_PASSWORD")
+        message=MIMEText(f"Your OTP for verification: {otp}")
+        message["subject"]="AI Study Planner OTP"
+        message["From"]=sender
+        message["To"]=email
+        server=smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(sender,password)
+        server.sendmail(sender,email,message.as_string())
+        server.quit()
+        return True
+    except Exception as e:
+        print(f"Error sending OTP: {e}")
+        return False
